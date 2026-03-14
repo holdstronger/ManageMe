f4309cdbb8e18a9a08a0d7af6e7218eeba3f42bc
Filip <holdstronger@users.noreply.github.com>
2026-03-14 14:38:00 +0100

    Lab2

--- src/api/storageKey.ts
+++ src/api/storageKey.ts
@@ -1,1 +1,3 @@
 export const PROJECTS_STORAGE_KEY = 'projekthub_projects'
+export const ACTIVE_PROJECT_STORAGE_KEY = 'projekthub_active_project'
+export const HISTORYJKI_STORAGE_KEY = 'projekthub_historyjki'
